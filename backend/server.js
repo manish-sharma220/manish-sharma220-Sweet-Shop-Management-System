c6da4460cca53246dbb51783453a038d10f383a6
Manish Kumar Sharma <sharmamanishkumar558@gmail.com>
2025-11-15 18:01:39 +0530

    Add backend code

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./db.js";
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Default Route
+app.get("/", (req, res) => {
+  res.send("Sweet Shop API is running...");
+});
+
+// Connect to Database
+connectDB();
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
